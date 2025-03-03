1d7dcb714afabf2d9502113c4ae9f90e3c101410
ISHIMWE JEAN LUC <167905435+ishimwejeanluc@users.noreply.github.com>
2025-03-04 00:35:47 +0200

    final commit

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/gadgets', authenticateToken, gadgetRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to IMF GADGET CHALLENGE. Check the API documentation .');
+});
+
 app.listen(3000, async () => {
     await sequelize.sync({ alter: true });
     console.log('Server running on port 3000');
